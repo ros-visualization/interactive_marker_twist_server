4b7115b5a665cb99155c0053d616fc4e038ea8bb
Tony Baltovski <tony.baltovski@gmail.com>
2015-07-08 16:29:24 -0400

    Added additional degrees of freedom (YZ) to linear interactive markers.

--- src/marker_server.cpp
+++ src/marker_server.cpp
@@ -36,6 +36,7 @@
 
 #include <algorithm>
 #include <string>
+#include <map>
 
 using visualization_msgs::InteractiveMarker;
 using visualization_msgs::InteractiveMarkerControl;
@@ -52,13 +53,22 @@ class MarkerServer
       nh.param<std::string>("link_name", link_name, "/base_link");
       nh.param<std::string>("robot_name", robot_name, "robot");
 
-      nh.param<double>("linear_scale", linear_drive_scale, 1.0);
-      nh.param<double>("angular_scale", angular_drive_scale, 2.2);
-      nh.param<double>("marker_size_scale", marker_size_scale, 1.0);
+      if (nh.getParam("linear_scale", linear_drive_scale_map))
+      {
+        nh.getParam("linear_scale", linear_drive_scale_map);
+        nh.getParam("max_positive_linear_velocity", max_positive_linear_velocity_map);
+        nh.getParam("max_negative_linear_velocity", max_negative_linear_velocity_map);
+      }
+      else
+      {
+        nh.param<double>("linear_scale", linear_drive_scale_map["x"], 1.0);
+        nh.param<double>("max_positive_linear_velocity", max_positive_linear_velocity_map["x"],  1.0);
+        nh.param<double>("max_negative_linear_velocity", max_negative_linear_velocity_map["x"], -1.0);
+      }
 
-      nh.param<double>("max_positive_linear_velocity", max_positive_linear_velocity, 1.0);
-      nh.param<double>("max_negative_linear_velocity", max_negative_linear_velocity, -1.0);
+      nh.param<double>("angular_scale", angular_drive_scale, 2.2);
       nh.param<double>("max_angular_velocity", max_angular_velocity, 2.2);
+      nh.param<double>("marker_size_scale", marker_size_scale, 1.0);
 
       vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
       createInteractiveMarkers();
@@ -76,10 +86,11 @@ class MarkerServer
     ros::Publisher vel_pub;
     interactive_markers::InteractiveMarkerServer server;
 
-    double linear_drive_scale;
+    std::map<std::string, double> linear_drive_scale_map;
+    std::map<std::string, double> max_positive_linear_velocity_map;
+    std::map<std::string, double> max_negative_linear_velocity_map;
+
     double angular_drive_scale;
-    double max_positive_linear_velocity;
-    double max_negative_linear_velocity;
     double max_angular_velocity;
     double marker_size_scale;
 
@@ -90,18 +101,32 @@ class MarkerServer
 void MarkerServer::processFeedback(
     const InteractiveMarkerFeedback::ConstPtr &feedback )
 {
+  geometry_msgs::Twist vel;
+
   // Handle angular change (yaw is the only direction in which you can rotate)
   double yaw = tf::getYaw(feedback->pose.orientation);
-
-  geometry_msgs::Twist vel;
   vel.angular.z = angular_drive_scale * yaw;
-  vel.linear.x = linear_drive_scale * feedback->pose.position.x;
-
-  // Enforce parameterized speed limits
-  vel.linear.x = std::min(vel.linear.x, max_positive_linear_velocity);
-  vel.linear.x = std::max(vel.linear.x, max_negative_linear_velocity);
-  vel.angular.z = std::min(vel.angular.z, max_angular_velocity);
+  vel.angular.z = std::min(vel.angular.z,  max_angular_velocity);
   vel.angular.z = std::max(vel.angular.z, -max_angular_velocity);
+
+  if (linear_drive_scale_map.find("x") != linear_drive_scale_map.end())
+  {
+    vel.linear.x = linear_drive_scale_map["x"] * feedback->pose.position.x;
+    vel.linear.x = std::min(vel.linear.x, max_positive_linear_velocity_map["x"]);
+    vel.linear.x = std::max(vel.linear.x, max_negative_linear_velocity_map["x"]);
+  }
+  if (linear_drive_scale_map.find("y") != linear_drive_scale_map.end())
+  {
+    vel.linear.y = linear_drive_scale_map["y"] * feedback->pose.position.y;
+    vel.linear.y = std::min(vel.linear.y, max_positive_linear_velocity_map["y"]);
+    vel.linear.y = std::max(vel.linear.y, max_negative_linear_velocity_map["y"]);
+  }
+  if (linear_drive_scale_map.find("z") != linear_drive_scale_map.end())
+  {
+    vel.linear.z = linear_drive_scale_map["z"] * feedback->pose.position.z;
+    vel.linear.z = std::min(vel.linear.z, max_positive_linear_velocity_map["z"]);
+    vel.linear.z = std::max(vel.linear.z, max_negative_linear_velocity_map["z"]);
+  }
 
   vel_pub.publish(vel);
 
@@ -123,13 +148,37 @@ void MarkerServer::createInteractiveMarkers()
   InteractiveMarkerControl control;
 
   control.orientation_mode = InteractiveMarkerControl::FIXED;
-  control.orientation.w = 1;
-  control.orientation.x = 1;
-  control.orientation.y = 0;
-  control.orientation.z = 0;
-  control.name = "move_x";
-  control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-  int_marker.controls.push_back(control);
+
+  if (linear_drive_scale_map.find("x") != linear_drive_scale_map.end())
+  {
+    control.orientation.w = 1;
+    control.orientation.x = 1;
+    control.orientation.y = 0;
+    control.orientation.z = 0;
+    control.name = "move_x";
+    control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
+    int_marker.controls.push_back(control);
+  }
+  if (linear_drive_scale_map.find("y") != linear_drive_scale_map.end())
+  {
+    control.orientation.w = 1;
+    control.orientation.x = 0;
+    control.orientation.y = 0;
+    control.orientation.z = 1;
+    control.name = "move_y";
+    control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
+    int_marker.controls.push_back(control);
+  }
+  if (linear_drive_scale_map.find("z") != linear_drive_scale_map.end())
+  {
+    control.orientation.w = 1;
+    control.orientation.x = 0;
+    control.orientation.y = 1;
+    control.orientation.z = 0;
+    control.name = "move_z";
+    control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
+    int_marker.controls.push_back(control);
+  }
 
   control.orientation.w = 1;
   control.orientation.x = 0;
@@ -139,14 +188,7 @@ void MarkerServer::createInteractiveMarkers()
   control.interaction_mode = InteractiveMarkerControl::ROTATE_AXIS;
   int_marker.controls.push_back(control);
 
-  // Commented out for non-holonomic robot. If holonomic, can move in y.
-  /*control.orientation.w = 1;
-  control.orientation.x = 0;
-  control.orientation.y = 0;
-  control.orientation.z = 1;
-  control.name = "move_y";
-  control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-  int_marker.controls.push_back(control);*/
+
 
   server.insert(int_marker, boost::bind(&MarkerServer::processFeedback, this, _1));
 
